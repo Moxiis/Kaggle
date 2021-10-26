6510d29fe55dba1c38f0fd01b83e9f1cf582672a
Mateusz <66701870+Moxiis@users.noreply.github.com>
2021-10-26 16:55:34 +0200

    Create Autosave_script.js

---
+++ Autosave_script.js
@@ -0,0 +1,7 @@
+function autosave(){
+document.getElementsByClassName("sc-psCJM duQXbd sc-jxtrTG aXiXj")[0].click();
+document.getElementsByClassName("sc-pZAwj kCyJhb")[0].click()
+document.getElementsByClassName("sc-fzqARJ sc-fzqNqU fTMsNj")[4].click()
+document.getElementsByClassName("sc-psCJM jciFbB SaveVersionBasicModal_StyledButton-sc-1nw2i1e fByxVM")[0].click()
+}
+setInterval(autosave, 2000000) //adjust to your preferences	
