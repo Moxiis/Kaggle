672ac8ad164432e99f02f27a89a51dfe4f674dbe
Mateusz <66701870+Moxiis@users.noreply.github.com>
2021-10-26 17:00:47 +0200

    Update Autosave_script.js

--- Autosave_script.js
+++ Autosave_script.js
@@ -1,7 +1,8 @@
+// Invoke the developer tool(f12) and paste this code into the "Console" tab
 function autosave(){
 document.getElementsByClassName("sc-psCJM duQXbd sc-jxtrTG aXiXj")[0].click();
 document.getElementsByClassName("sc-pZAwj kCyJhb")[0].click()
 document.getElementsByClassName("sc-fzqARJ sc-fzqNqU fTMsNj")[4].click()
 document.getElementsByClassName("sc-psCJM jciFbB SaveVersionBasicModal_StyledButton-sc-1nw2i1e fByxVM")[0].click()
 }
-setInterval(autosave, 2000000) //adjust to your preferences	
+setInterval(autosave, 2000000) // adjust time to your preferences	
